3aeb935e62555b4391d016ca0709d81b0d12a640
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-11-04 00:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,40 @@
         myCat.makeSound(); // Output: The Siamese makes a sound
         myCat.purr();      // Output: purr
 
-        const myDog = new Dog("Golden Retriever");
+        const myDog = new Doclass Animal {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example Usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr();      // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark();      // Output: woof
+g("Golden Retriever");
         myDog.makeSound(); // Output: The Golden Retriever makes a sound
         myDog.bark();   
